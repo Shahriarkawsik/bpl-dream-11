314a5d41ea1e3c3c3cdee0ac3b6c54e9c9ca2b68
Shahriar <shahriarkawsik@gmail.com>
2024-11-01 16:02:19 +0600

    add navbar

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import icon from '../../assets/image/logo.png'
+import coin from '../../assets/image/coin.png'
+const Header = () => {
+  return (
+    <header className='w-10/12 sm:w-10/12 lg:w-4/5 mx-auto mt-12'>
+      {/* backdrop-blur-md fixed top-0 w-full z-10 */}
+    <nav className='flex justify-between items-center backdrop-blur-md fixed w-10/12 sm:w-10/12 lg:w-4/5 mx-auto z-10'>
+      <img src={icon} alt="" />
+      <ul className='flex items-center sm:space-x-8 lg:space-x-12 max-sm:hidden'>
+        <li className='leading-5 text-color1.7'>Home</li>
+        <li className='leading-5 text-color1.7'>Fixture</li>
+        <li className='leading-5 text-color1.7'>Teams</li>
+        <li className='leading-5 text-color1.7'>Schedules</li>
+      </ul>
+      <button className='flex justify-between items-center px-5 py-4 border border-color1.1 text-color1 rounded-xl '>
+          <span className='mr-2'>0</span> Coin <img className='w-5 h-5 ml-2' src={coin} alt="a coin img" />
+      </button>
+    </nav>      
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'color1': 'rgb(19, 19, 19)',
+        'color1.1': 'rgba(19, 19, 19,0.1)',
+        'color1.05': 'rgba(19, 19, 19,0.05)',
+        'color1.7': 'rgba(19, 19, 19,0.7)',
+        'color1.6': 'rgba(19, 19, 19,0.6)',
+        'color1.4': 'rgba(19, 19, 19,0.4)',
+        'color2' : 'rgb(231, 254, 41)',
+      }
+    },
   },
   plugins: [],
 }
